0551cd5db84a48863dbfd7ebf897eb1562203a9d
Umkamaks <34101565+Umkamaks@users.noreply.github.com>
2017-11-29 18:17:58 +0200

    Update NewsContext.cs

--- DAL/EF/NewsContext.cs
+++ DAL/EF/NewsContext.cs
@@ -29,7 +29,7 @@ namespace DAL.EF
 
             context.Users.Add(new User
             {
-                Email = "Admin@admin.net",
+                Email = "Admin@admin.com",
                 Password = "Admin",
                 RoleId = 1
             });
